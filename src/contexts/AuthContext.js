a65adccf46282ea8d4e8c0fc6379d44af3262b39
unknown <shohorabshanto@gmail.com>
2022-04-25 16:51:34 +0600

    git-commit

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,16 +5,18 @@ import "../firebase";
 const AuthContext = React.createContext();
 //useAuth is a custom hook
 //ei custom hook provider er vitor ja value dibo oigula peye jabe eta call korle
-export function useAuth(){
+export function useAuth() { //component akhon useauth use korle use context er value peye jabe 
+    //bar bar alada alada component e use context import export kora lgbe na
+    //akhon use context use korle authcontext auto peye jbe
     return useContext(AuthContext);
 }
 
-export function AuthProvider({children}){
+export function AuthProvider({ children }) { //auth provider diea jesob component k amara rap korbo oigula ke amra child hisebe pabo
     const [loading, setLoading] = useState(true);
     const [currentUser, setCurrentUser] = useState();
 
     //signup function
-    
+
 
     return (
         <AuthContext.Provider value={value}>
