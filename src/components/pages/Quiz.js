84375d7efba7d05e6c67dc43693d892e6237a4d2
unknown <shohorabshanto@gmail.com>
2022-05-09 17:42:30 +0600

    questions

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
+import useQuestions from "../../hooks/useQuestions";
 import Answers from "../Answers";
 import MiniPlayer from "../MiniPlayer";
 import ProgressBar from "../ProgressBar";
 
+const initialState = null;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "questions":
+      action.vlaue.forEach((question) => {
+        question.options.foreach((option) => {
+          option.checked = false;
+        });
+      });
+      return action.value;
+    default:
+      return state;
+  }
+};
+
 export default function Quiz() {
   const { id } = useParams();
-  // const { loading, error, questions } = useQuestion(id);
+  const { loading, error, questions } = useQuestions(id);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [qna, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
--- src/hooks/useQuestions.js
+++ src/hooks/useQuestions.js
@@ -1,7 +1,7 @@
 import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 
-export default function useQuestionList(videoID) {
+export default function useQuestions(videoID) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [questions, setQuestions] = useState([]);
@@ -32,7 +32,7 @@ export default function useQuestionList(videoID) {
       }
     }
     setTimeout(() => {
-      fetchVideos();
+      fetchQuestions();
     }, 2000);
   }, [videoID]);
   return {
